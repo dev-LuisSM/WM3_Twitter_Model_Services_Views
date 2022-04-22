f4dc21b3c8c2cd8fd92da4630dbda9294211403b
luiseduardo.silva.contractor <luiseduardo.silva.contractor@bbva.com>
2022-04-21 19:35:17 -0500

    4) Changing the string date to Date()

--- twitter/test/models/User.test.js
+++ twitter/test/models/User.test.js
@@ -2,13 +2,15 @@ const User = require('./../../app/models/User')
 
 describe('Unit Test for User class', () => {
   test('Create an User object', () => {
-    const user = new User(1, "dev_LuisSM", "Luis", "Bio", "dateCreated", "lastUpdated")
+    const user = new User(1, "dev_LuisSM", "Luis", "Bio")
 
     expect(user.id).toBe(1)
     expect(user.username).toBe("dev_LuisSM")
     expect(user.name).toBe("Luis")
     expect(user.bio).toBe("Bio")
-    expect(user.dateCreated).toBe("dateCreated")
-    expect(user.lastUpdated).toBe("lastUpdated")
+    // Setting the date
+    // not.toBeUndefined verifica que el valor no sea Undefined
+    expect(user.dateCreated).not.toBeUndefined()
+    expect(user.lastUpdated).not.toBeUndefined()
   })
 })
