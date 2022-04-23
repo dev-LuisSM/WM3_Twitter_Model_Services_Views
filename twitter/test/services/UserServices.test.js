fbf422d422783376c099b25f16535eda6759d404
luiseduardo.silva.contractor <luiseduardo.silva.contractor@bbva.com>
2022-04-23 17:54:39 -0500

    10) Last req. getAllUsernames

--- twitter/test/services/UserServices.test.js
+++ twitter/test/services/UserServices.test.js
@@ -24,4 +24,14 @@ describe('Test for UserService', () => {
     UserService.updateUserUsername(user, 'not_dev_LuisSM')
     expect(user.username).toBe('not_dev_LuisSM')
   })
+
+  test('4) Given a list of users give me the list of usernames', () => {
+    const user1 = UserService.create(1, "dev_LuisSM1", "Luis")
+    const user2 = UserService.create(1, "dev_LuisSM2", "Luis")
+    const user3 = UserService.create(1, "dev_LuisSM3", "Luis")
+    const usernames = UserService.getAllUsernames([user1, user2, user3])
+    expect(usernames).toContain("dev_LuisSM1")
+    expect(usernames).toContain("dev_LuisSM2")
+    expect(usernames).toContain("dev_LuisSM3")
+  })
 })
