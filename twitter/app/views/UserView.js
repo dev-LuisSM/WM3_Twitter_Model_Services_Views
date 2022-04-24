5d751899aad059655ea0b6856f4b6010c2dea5fd
luiseduardo.silva.contractor <luiseduardo.silva.contractor@bbva.com>
2022-04-23 21:47:48 -0500

    12) test for missing and/or invaled properties

--- twitter/app/views/UserView.js
+++ twitter/app/views/UserView.js
@@ -3,7 +3,9 @@ const UserService = require('./../../app/services/UserService')
 class UserView {
   static createUser(payload) {
     return payload === null ? { error: "Error: payload no existe" }
-    : ``
+      : (typeof payload.username != 'string' || typeof payload.name != 'string' || typeof payload.id != 'number')
+        ? { error: "Error los datos del payload necesitan tener un valor válido" }
+      : ``
   }
 }
 
--- twitter/test/views/UserView.test.js
+++ twitter/test/views/UserView.test.js
@@ -7,4 +7,15 @@ describe('Test for UserView', () => {
     expect(result.error).toMatch(/payload no existe/)
   })
 
+  test('Return an error object when try to create a new user with a payload with invalid properties', () => {
+    const payload = { username: null, name: 12, id: "id" }
+    const result = UserView.createUser(payload)
+    expect(result.error).toMatch(/necesitan tener un valor válido/)
+  })
+
+  test('Return an error object when try to create a new user with a payload with missing properties', () => {
+    const payload = { username: "Username" }
+    const result = UserView.createUser(payload)
+    expect(result.error).toMatch(/necesitan tener un valor válido/)
+  })
 })
